6108a0dd06da090ec75825703a7084598dd007cc
Sergio99778 <sergio4brandon@gmail.com>
2021-12-03 17:14:40 -0600

    Adding styled-Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const HeaderContainer = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: 'Slabo 27px', sans-serif;
+  text-align: center;
+  letter-spacing: 2px;
+`;
 
 const Header = () => {
   return (
-    <div>
-      <h1>Insurance quote</h1>
-    </div>
+    <HeaderContainer>
+      <HeaderText>Insurance quote</HeaderText>
+    </HeaderContainer>
   );
 };
 
