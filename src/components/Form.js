cb98bfcf2d2fd3f69e4d73a0282b91dd003b1ee8
Sergio99778 <sergio4brandon@gmail.com>
2021-12-03 17:44:50 -0600

    Adding Field.style Label.styled Select.styled Input.styled to Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,60 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+//Styles
+const Field = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Button = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 0.3s ease;
+  margin-top: 2rem;
+
+  &:hover {
+    background-color: #26c6da;
+    cursor: pointer;
+  }
+`;
 
 const Form = () => {
   return (
     <form>
-      <div className="">
-        <label htmlFor="">Model: </label>
-        <select name="" id="">
+      <Field className="">
+        <Label htmlFor="">Model: </Label>
+        <Select name="" id="">
           <option value="">-- Option --</option>
           <option value="american">American</option>
           <option value="european">European</option>
           <option value="asian">Asian</option>
-        </select>
-      </div>
-      <div className="">
-        <label htmlFor="">Year: </label>
-        <select name="" id="">
+        </Select>
+      </Field>
+      <Field className="">
+        <Label htmlFor="">Year: </Label>
+        <Select name="" id="">
           <option>-- Option --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -23,15 +63,16 @@ const Form = () => {
           <option value="2017">2017</option>
           <option value="2016">2016</option>
           <option value="2015">2015</option>
-        </select>
-      </div>
-      <div className="">
-        <label htmlFor="">Plan: </label>
-        <input type="radio" name="plan" id="" value="basic" />
+        </Select>
+      </Field>
+      <Field className="">
+        <Label htmlFor="">Plan: </Label>
+        <InputRadio type="radio" name="plan" id="" value="basic" />
         Basic
-        <input type="radio" name="plan" id="" value="complete" />
-      </div>
-      <button type="button">Quote</button>
+        <InputRadio type="radio" name="plan" id="" value="complete" />
+        Complete
+      </Field>
+      <Button type="button">Quote</Button>
     </form>
   );
 };
